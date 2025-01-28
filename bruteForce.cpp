703617c69580ca0ad6903a716e979d8120c8c65c
WendellReis <79986893+WendellReis@users.noreply.github.com>
2025-01-28 11:29:39 -0300

    Mecanismo de tempo e otimizações

--- bruteForce.cpp
+++ bruteForce.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <unordered_set>
 #include <algorithm>
 #include <string>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
-unordered_set<int> MDS;
+vector<bool> MDS;
+int MDS_size = 0;
 
-void inicializeMDS(int size) {
-   for (size_t i = 1; i <= size; i++)
-        MDS.insert(i);
+void displayMDS() {
+    cout << "MDS ("<< MDS_size << ") = [";
+    bool first = true;
+    for (int i = 1; i < MDS.size(); i++) {
+        if (MDS[i]) {
+            if (!first) cout << ", ";
+            cout << i;
+            first = false;
+        }
+    }
+    cout << "]\n";
 }
 
-bool isDominant(unordered_set<int> &ds, vector<vector<int>> &adj, int size) {
+
+bool isDominant(vector<bool> &ds, vector<vector<int>> &adj, int size) {
     for(int v = 1; v <= size; v++) {
-        if(ds.find(v) != ds.end())
+        if(ds[v])
             continue;
         
         bool dominated = false;
         for(auto u : adj[v])
-            if(ds.find(u) != ds.end()) {
+            if(ds[u]) {
                 dominated = true;
                 break;
             }
@@ -31,32 +42,28 @@ bool isDominant(unordered_set<int> &ds, vector<vector<int>> &adj, int size) {
     return true;
 }
 
-bool updateDominant(unordered_set<int> &ds) {
-    if(ds.size() > MDS.size())
+bool updateDominant(vector<bool> &ds, int sizeds) {
+    if(sizeds >= MDS_size)
         return false;
 
     MDS = ds;
+    MDS_size = sizeds;
+    displayMDS();
     return true;
 }
 
-void solve(unordered_set<int> ds, vector<vector<int>> &adj, int size, int idx = 0) {
-    if(ds.size() > MDS.size()) {
-        if(isDominant(ds,adj,size))
-            updateDominant(ds);
+void solve(vector<bool> ds, vector<vector<int>> &adj,int size, int sizeds = 0, int idx = 0) {
+    if (sizeds >= MDS_size) return;
+
+    if (isDominant(ds, adj, size)) {
+        updateDominant(ds, sizeds);
         return;
     }
 
-    for(int v = idx+1; v <= size; v++) {
-        if (ds.size() >= MDS.size() && !MDS.empty()) return;
-
-        if (isDominant(ds, adj, size)) {
-            updateDominant(ds);
-            return;
-        }
-        
-        ds.insert(v);
-        solve(ds,adj,size,v);
-        ds.erase(v);
+    for (int v = idx; v <= size; v++) {
+        ds[v] = true;
+        solve(ds, adj, size, sizeds + 1, v + 1);
+        ds[v] = false;
     }
 }
 
@@ -97,6 +104,7 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
+
     for(int i = 1; i < argc; i++) {
         vector<vector<int>> adj = getGraph(argv[i]);
         if(adj.size() == 0) {
@@ -104,15 +112,29 @@ int main(int argc, char* argv[]) {
             continue;
         }
 
+        auto start = high_resolution_clock::now();
+   
         int size = adj.size()-1;
+        vector<bool> ds(size+1,0);
+        MDS = vector<bool>(size+1,1);
+        MDS_size = size;
+        solve(ds,adj,MDS_size);
 
-        unordered_set<int> ds;
-        inicializeMDS(size);
-        solve(ds,adj,size);
-        printf("MDS = %d : [ ", (int)MDS.size());
-        for(auto v : MDS)
-            cout << v << " ";
-        cout << "]\n";
+        auto stop = high_resolution_clock::now();
+
+        auto duration = duration_cast<seconds>(stop - start);
+        long long duration_ms = duration_cast<milliseconds>(stop - start).count();
+        
+        long long hours = duration_ms / 3600000;
+        long long minutes = (duration_ms % 3600000) / 60000;
+        long long seconds = (duration_ms % 60000) / 1000;
+
+        cout << "Filename: " << argv[i] << endl;
+        displayMDS();
+        cout << "Tempo de execucao: " 
+            << hours << " horas, "
+            << minutes << " minutos e "
+            << seconds << " segundos." << endl;
     }
 
     return 0;
