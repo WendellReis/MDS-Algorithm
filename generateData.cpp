bce3e947ed375ef111c8735e5bf010df523f81a0
¨WendellReis¨ <wendellreismatias@gmail.com>
2025-02-05 20:31:10 -0300

    Gerador de instancias aleatorias

---
+++ generateData.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#define SEED 1234
+#define PATH "archives/"
+
+int main() {
+    srand(SEED);
+
+    vector<int> SIZES = {15, 20, 25, 30};
+
+    for(auto s : SIZES) {
+        string filename = string(PATH) + "g" + to_string(s) + ".txt";
+        ofstream file(filename);
+        if(!file.is_open()) {
+            cerr << "Erro ao abrir o arquivo: " << filename << endl;
+            break;
+        }
+
+        file << s << endl;
+        for(int i = 1; i <= s; i++)
+            for(int j = i+1; j <= s; j++)
+                if(rand()%2)
+                    file << i << " " << j << endl;
+        file.close();
+    }
+
+    return 0;
+}
